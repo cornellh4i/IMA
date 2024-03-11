d1b5cc9dcd134e42e2fa448b4761a546d829da73
JJBai2003 <jiayibai2003@gmail.com>
2024-03-11 07:46:28 -0400

    Added some changes to the main page

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,15 @@
-import { useEffect, useState } from 'react';
-
-import MultiActionAreaCard from "./components/card.js";
-import Headers from "./components/header.js";
-import SearchBar from "./components/search.js";
-import LandingPage from './components/landing.js';
 
 const API_URL = process.env.REACT_APP_API;
 
 
 function App() {
-  const [data, setData] = useState('No data :(');
-
-  useEffect(() => {
-    async function getData() {
-      const url = `${API_URL}/hello/`;
-      const response = await fetch(url);
-      const data = await response.json();
-      setData(data.msg);
-    }
-    getData();
-  }, []);
-
-  const [show, setShow] = useState(false);
-
-  const handleOpen = () => setShow(true);
-  const handleClose = () => setShow(false);
-
-
-
-  return (
+  return(
     <>
-      <>
-        <Headers />
-      </>
-      <SearchBar />
-      <>
-        <LandingPage />
-      </>
+    <div>
+      <div>Hello world</div>
+
+    </div>
     </>
-  );
+  )
 }
 export default App;
