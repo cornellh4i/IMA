352095ab1238f4fbbd063e341cb084841695abc8
sedhainr2004 <sedhainrohan@gmail.com>
2024-03-23 16:24:58 -0400

    added the instructions from ticket

---
+++ server/src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+app.use(express.json());
+mongoose.connect(
+  "mongodb+srv://h4i-ima:h4iIMA2024@ima.pejjbfw.mongodb.net/",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (!err) {
+      console.log("connected to db");
+    } else {
+      console.log(err);
+    }
+  }
+);
+app.listen(3000, () => {
+  console.log("on port 3000");
+});
