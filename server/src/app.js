0117db6a30ec34c006c86742ea073f3bc6d2a29c
phoebe-qian <pqian7@gmail.com>
2024-03-23 16:40:21 -0400

    made user model

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,10 @@
 const express=require('express');
 const app = express();
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const users = mongoose.model('users', usersSchema);
+
+
 app.use(express.json())
 mongoose.connect("mongodb+srv://h4i-ima:h4iIMA2024@ima.pejjbfw.mongodb.net/", {
     useNewUrlParser: true,
@@ -16,3 +20,16 @@ mongoose.connect("mongodb+srv://h4i-ima:h4iIMA2024@ima.pejjbfw.mongodb.net/", {
 			app.listen(3000, () => {
     				console.log("on port 3000")
 })
+
+const usersSchema = new Schema({
+    name: String, // String is shorthand for {type: String}
+    email: String,
+    id: Number,
+    comments: [{ body: String, date: Date }],
+    date: { type: Date, default: Date.now },
+    hidden: Boolean,
+    meta: {
+      votes: Number,
+      favs: Number
+    }
+  });
