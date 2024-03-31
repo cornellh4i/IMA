2ef501aa10e9af2da42a38dfaa49e430707a7bc2
JJ <60070699+JJBai2003@users.noreply.github.com>
2024-03-31 15:53:51 -0400

    merged frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/header.js';
 import Sidebar from "./components/Sidebar";
-import SearchBar from './components/searchBar.js';import Sidebar from "./components/Sidebar";
+import SearchBar from './components/searchBar.js';
+import './App.css'
 const API_URL = process.env.REACT_APP_API;
 
 function App() {
@@ -8,8 +9,10 @@ function App() {
     <>
     <div class = "container">
       <Header/>
-      <SearchBar/>
-      <Sidebar />
+      <div class = "top">
+        <Sidebar />
+        <SearchBar/>
+      </div>
     </div>
     </>
   );
