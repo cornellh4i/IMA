06b9e49ddbce118416308c8655af3848fb61c4a8
JJ <jiayibai2003@gmail.com>
2024-04-29 15:10:50 -0400

    fixed index.js

--- server/src/index.js
+++ server/src/index.js
@@ -2,11 +2,9 @@ const express = require("express");
 const cors = require('cors');
 const app = express();
 const mongoose = require("mongoose");
-const cors = require("cors");
 const { Schema } = mongoose;
 app.use(cors());
 app.use(express.json());
-app.use(cors());
 
 
 mongoose.connect(
