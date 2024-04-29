8e6936984ef9a5ce0a4d2d9d808f7cf7c38c1e42
JJ <jiayibai2003@gmail.com>
2024-04-29 15:21:17 -0400

    fixed some merging bugs

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import Sidebar from "./components/Sidebar.js";
 import AddMemberPage from "./components/AddMemberPage.js";
 import SearchBar from "./components/searchBar.js";
-import SearchBar from "./components/searchBar.js";
 import Card from "./components/Card";
 import Header from "./components/header.js";
 import "./App.css";
@@ -16,10 +15,10 @@ function App() {
       <div className="container">
         <Header />
         <div className="top">
-          <Sidebar setMembers={setMembers} />
+          <Sidebar setMembers={SearchBar.setMembers} />
           <div className="middle">
             <SearchBar />
-            <div className="cards">{members.map(createCard)}</div>
+            {/* <div className="cards">{members.map(createCard)}</div> */}
           </div>
         </div>
         <AddMemberPage />
