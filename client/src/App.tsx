050078e32f3abedcf23cbf4e4b5c6c18ebb0eb4d
brandon-lerit <brandon338@icloud.com>
2025-02-28 21:40:26 -0500

    Added Test Page for Client Side

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,55 +1,17 @@
-import React, { useState, useEffect } from "react";
-import Sidebar from "./components/Sidebar.tsx";
-import AddMemberPage from "./components/AddMemberPage.tsx";
-import SearchBar from "./components/SearchBar.tsx";
-import Card from "./components/Card.tsx";
-import Header from "./components/Header.tsx";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import TestPage from "./pages/TestPage.tsx";
 import "./App.css";
 
-// Type for the members object, you should define it according to the actual structure of members.
-interface Member {
-  id: number;
-  name: string;
-  year: string;
-  role: string;
-  image: string;
-  email?: string;
-  linkedin?: string;
-  slack?: string;
-}
-
-const API_URL = process.env.REACT_APP_API;
-
 const App: React.FC = () => {
-  const [members, setMembers] = useState<Member[]>([]); // Specify Member[] for members
-  const [isModalOpen, setModalOpen] = useState<boolean>(false); // Specify boolean type for modal state
-
-  const handleOpenModal = () => setModalOpen(true);
-  const handleCloseModal = () => setModalOpen(false);
-
-  useEffect(() => {
-    fetch(`${API_URL}/getMember`)
-      .then((res) => res.json())
-      .then((data) => {
-        setMembers(data);
-      })
-      .catch((error) => console.error("Error fetching members:", error));
-  }, []); // Empty dependency array for once on component mount
-
   return (
-    <>
-      <div className="container">
-        <Header onAddMemberClick={handleOpenModal} />
-        <div className="top">
-          <Sidebar setMembers={setMembers} />
-          <div className="middle">
-            <SearchBar />
-            {/* Render cards if members exist */}
-          </div>
-        </div>
-        <AddMemberPage isOpen={isModalOpen} onClose={handleCloseModal} />
-      </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/test" element={<TestPage />} />
+      </Routes>
+    </Router>
   );
 };
 
