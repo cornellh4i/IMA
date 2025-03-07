0a9a171a5440b4349444ad4d8401b11f7aee1679
brandon-lerit <brandon338@icloud.com>
2025-03-06 20:34:05 -0500

    Split index.ts into app & index

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,9 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
+import app from "./app";
 import { connectDB } from "./database";
-import userRoutes from "./routes/user";
 
-dotenv.config();
 const PORT = process.env.PORT || 8000;
 
-const app = express();
-
 connectDB().then(() => {
-  app.use(cors());
-  app.use(express.json());
-
-  app.use("/api/users", userRoutes);
-
   app.listen(PORT, () => {
     console.log(`Server running on Port: ${PORT}`);
   });
