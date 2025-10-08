3964db0480fc915ead3e54477623a41ca60d7ca1
Chimdi Ejiogu <chimdije@gmail.com>
2025-10-08 14:47:57 -0400

    Implement mapping function from response body to member table schema

--- server/src/controllers/member.ts
+++ server/src/controllers/member.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { getAllMembersService } from "../services/memberServices";
 import { MEMBERS_TABLE, MemberFields } from "../models/member";
 import { supabase } from "../supabase";
+import { mapMemberToRow } from "../services/memberServices";
 
 /**
  * Get all members
@@ -24,18 +25,21 @@ export const getAllMembers = async (req: Request, res: Response): Promise<void> 
  */
 export const addMember = async (req: Request, res: Response): Promise<void> => {
   const member : MemberFields = req.body;
+
+  // Map the member to the Supabase row format (excluding the id and created_at fields)
+  const memberRow = mapMemberToRow(member);
   
   // Add the member to the database
   const { data, error } = await supabase
   .from(MEMBERS_TABLE)
-  .insert(member)
+  .insert(memberRow)
   .select() // Return the inserted member
   .single(); // Supabase returns an array of objects, so we use .single() to return the first object
   
   // If error, return 400 (supabase does not throw an error, just returns — no need for try catch)
   if (error) {
     console.error("Error adding member:", error);
-    res.status(400).json({ error: "Failed to add member" });
+    res.status(400).json({ error: "Failed to add member" + error.message });
     return;
   }
 
