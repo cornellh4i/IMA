333a906b0a1b15ecd2a959772d6ec401eb3f23a4
Chimdi Ejiogu <chimdije@gmail.com>
2025-10-21 21:00:49 -0400

    Implement route for getting session

--- client/src/pages/Auth.tsx
+++ client/src/pages/Auth.tsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import Header from "../components/Header.tsx";
+declare const process: any;
 
 const Auth: React.FC = () => {
-  const API_URL = process.env.REACT_APP_API || `http://localhost:8000/`;
+  // const API_URL = process.env.REACT_APP_API || `http://localhost:8000`;
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
 
   const logSession = async () => {
     // Get session from server
-    const session = await fetch(`${API_URL}/api/auth/session`);
+    const session = await fetch(`http://localhost:8000/api/auth/session`);
     const data = await session.json();
     console.log(data);
   }
